ca8445f9138d58c5dbadf97a9b7ee76466cafb81
Jimmy Larsson <jimmy.larsson@europe.com>
2024-06-25 10:49:57 +0200

    first commit

---
+++ src/main/java/org/example/stablecablelable/StableCableLableApplication.java
@@ -0,0 +1,13 @@
+package org.example.stablecablelable;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class StableCableLableApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(StableCableLableApplication.class, args);
+    }
+
+}
---
+++ src/test/java/org/example/stablecablelable/StableCableLableApplicationTests.java
@@ -0,0 +1,13 @@
+package org.example.stablecablelable;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class StableCableLableApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
